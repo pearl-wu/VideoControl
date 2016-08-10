7d54ebfaa0b1dc43c38c98fec6c02a1bae05f4b0
pearl <lu92500@yahoo.com.tw>
2016-08-10 11:22:46 +0800

    Add files via upload

--- www/videoplayer.js
+++ www/videoplayer.js
@@ -1,24 +1,13 @@
 var exec = require("cordova/exec");
 
 module.exports = {
-
-    DEFAULT_OPTIONS: {
-        volume: 1.0,
-        scalingMode: 1
+    
+    many_play: function (paths, number, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "VideoPlayer", "many_play", [paths, number]);
     },
-
-    SCALING_MODE: {
-        SCALE_TO_FIT: 1,
-        SCALE_TO_FIT_WITH_CROPPING: 2
-    },
-
-    play: function (path, options, successCallback, errorCallback) {
-        options = this.merge(this.DEFAULT_OPTIONS, options);
-        exec(successCallback, errorCallback, "VideoPlayer", "play", [path, options]);
-    },
-
-    close: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "VideoPlayer", "close", []);
+    
+    custom_play: function (paths, number, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "VideoPlayer", "custom_play", [paths, number]);
     },
 
     merge: function () {
@@ -30,5 +19,4 @@ module.exports = {
         });
         return obj;
     }
-
 };
